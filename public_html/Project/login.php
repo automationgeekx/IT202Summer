7f158ad97ecc25f6de7203a633a73e30adf47754
bg339 <bg339@njit.edu>
2022-07-07 03:38:29 -0400

    Feat-UserRoles

--- public_html/Project/login.php
+++ public_html/Project/login.php
@@ -29,28 +29,35 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     //TODO 3
     $hasError = false;
     if (empty($email)) {
-        flash("Email must not be empty", "danger");
+        flash("Email must not be empty");
         $hasError = true;
     }
     //sanitize
+    //$email = filter_var($email, FILTER_SANITIZE_EMAIL);
     $email = sanitize_email($email);
     //validate
+    /*if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        flash("Invalid email address");
+        $hasError = true;
+    }*/
     if (!is_valid_email($email)) {
-        flash("Invalid email address", "danger");
+        flash("Invalid email address");
         $hasError = true;
     }
     if (empty($password)) {
-        flash("password must not be empty", "danger");
+        flash("password must not be empty");
         $hasError = true;
     }
-    if (strlen($password) < 8) {
-        flash("Password too short", "danger");
+    if (!is_valid_password($password)) {
+        flash("Password too short");
         $hasError = true;
     }
     if (!$hasError) {
+        //flash("Welcome, $email");
         //TODO 4
         $db = getDB();
-        $stmt = $db->prepare("SELECT id, email, username, password from Users where email = :email");
+        $stmt = $db->prepare("SELECT id, email, username, password from Users 
+        where email = :email");
         try {
             $r = $stmt->execute([":email" => $email]);
             if ($r) {
@@ -59,14 +66,31 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
                     $hash = $user["password"];
                     unset($user["password"]);
                     if (password_verify($password, $hash)) {
-                        $_SESSION["user"] = $user;
-                        flash("Welcome " . get_username());
+                        //flash("Weclome $email");
+                        $_SESSION["user"] = $user; //sets our session data from db
+                        try {
+                            //lookup potential roles
+                            $stmt = $db->prepare("SELECT Roles.name FROM Roles 
+                        JOIN UserRoles on Roles.id = UserRoles.role_id 
+                        where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
+                            $stmt->execute([":user_id" => $user["id"]]);
+                            $roles = $stmt->fetchAll(PDO::FETCH_ASSOC); //fetch all since we'll want multiple
+                        } catch (Exception $e) {
+                            error_log(var_export($e, true));
+                        }
+                        //save roles or empty array
+                        if (isset($roles)) {
+                            $_SESSION["user"]["roles"] = $roles; //at least 1 role
+                        } else {
+                            $_SESSION["user"]["roles"] = []; //no roles
+                        }
+                        flash("Welcome, " . get_username());
                         die(header("Location: home.php"));
                     } else {
-                        flash("Invalid password", "danger");
+                        flash("Invalid password");
                     }
                 } else {
-                    flash("Email not found", "danger");
+                    flash("Email not found");
                 }
             }
         } catch (Exception $e) {
@@ -75,6 +99,5 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
     }
 }
 ?>
-<?php
-require(__DIR__ . "/../../partials/flash.php");
-?>
+<?php 
+require(__DIR__."/../../partials/flash.php");
